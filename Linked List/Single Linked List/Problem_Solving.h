f28f25c65f8bd6e3989fe2f8cbf26f2dae3af515
Mohammed Atef <mohamed45452020@gmail.com>
2025-06-04 17:10:39 +0300

    Added Problem_Solving files...

---
+++ Linked List/Single Linked List/Problem_Solving.h
@@ -0,0 +1,386 @@
+#ifndef SINGLE_LINKED_LIST_PROBLEM_SOLVING_H
+#define SINGLE_LINKED_LIST_PROBLEM_SOLVING_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+private:
+    int binaryToDecimal(const string& n){
+        string num = n;
+        int dec_value = 0;
+
+        int base = 1, len = num.size();
+        for (int i = len - 1; i >= 0; i--) {
+            if (num[i] == '1') dec_value += base;
+            base = base * 2;
+        }
+
+        return dec_value;
+    }
+
+    int findSize(ListNode *head) const {
+        ListNode *current = head;
+        int size = 0;
+        while (current) {
+            size++;
+            current = current->next;
+        }
+        return size;
+    }
+
+    int findSize(Node *head) const {
+        Node *current = head;
+        int size = 0;
+        while (current) {
+            size++;
+            current = current->next;
+        }
+        return size;
+    }
+
+public:
+    /*
+        * 234. Palindrome Linked List (Leetcode).
+        * Link: https://leetcode.com/problems/palindrome-linked-list/description/
+    */
+    bool isPalindrome(ListNode* head) {
+        string res;
+        ListNode * current = head;
+        if (!head->next) return true;
+
+        while (current){
+            res += to_string(current->val);
+            current = current->next;
+        }
+
+        for (int i = 0; i < res.size()/2; ++i) {
+            if (res[i] != res[res.size() -1-i]) return false;
+        }
+        return true;
+    }
+
+    /*
+        * 206. Reverse Linked List (Leetcode).
+        * Link: https://leetcode.com/problems/reverse-linked-list/description/
+    */
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return nullptr;
+
+        vector<int> temp;
+        ListNode *current = head;
+        while (current){
+            temp.push_back(current->val);
+            current = current->next;
+        }
+
+        ListNode *res = new ListNode(temp[temp.size() -1]);
+        ListNode *dummy = res;
+        for (int i = temp.size() -2; i >= 0; --i) {
+            dummy->next = new ListNode(temp[i]);
+            dummy = dummy->next;
+        }
+        return res;
+    }
+
+    /*
+        * 92. Reverse Linked List II (Leetcode).
+        * Link: https://leetcode.com/problems/reverse-linked-list-ii/description/
+    */
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head) return nullptr;
+
+        vector<int> temp;
+        ListNode *current = head;
+        while (current){
+            temp.push_back(current->val);
+            current = current->next;
+        }
+
+        reverse(temp.begin() + left - 1, temp.begin() + right);
+
+        ListNode *res = new ListNode(temp[0]);
+        ListNode *dummy = res;
+        for (int i = 1; i < temp.size(); ++i) {
+            dummy->next = new ListNode(temp[i]);
+            dummy = dummy->next;
+        }
+        return res;
+    }
+
+    /*
+        * 21. Merge Two Sorted Lists (Leetcode).
+        * Link: https://leetcode.com/problems/merge-two-sorted-lists/description/
+    */
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1 && !list2) return nullptr;
+
+        vector<int> temp;
+        ListNode *current1 = list1;
+        ListNode *current2 = list2;
+        while (current1){
+            temp.push_back(current1->val);
+            current1 = current1->next;
+        }
+        while (current2){
+            temp.push_back(current2->val);
+            current2 = current2->next;
+        }
+
+        sort(temp.begin(), temp.end());
+
+        ListNode *res = new ListNode(temp[0]);
+        ListNode *dummy = res;
+        for (int i = 1; i < temp.size(); ++i) {
+            dummy->next = new ListNode(temp[i]);
+            dummy = dummy->next;
+        }
+        return res;
+    }
+
+    /*
+        * 876. Middle of the Linked List (Leetcode).
+        * Link: https://leetcode.com/problems/merge-two-sorted-lists/description/
+    */
+    ListNode* middleNode(ListNode* head) {
+        if (!head) return nullptr;
+
+        vector<ListNode*> temp;
+        ListNode *current = head;
+        while (current){
+            temp.push_back(current);
+            current = current->next;
+        }
+
+        int middle = temp.size() /2;
+        ListNode *res = head;
+        while(middle--)
+            res = res->next;
+        return res;
+    }
+
+    /*
+        * 1290. Convert Binary Number in a Linked List to Integer (Leetcode).
+        * Link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+    */
+    int getDecimalValue(ListNode* head) {
+        string res;
+        ListNode * current = head;
+        while (current){
+            res += to_string(current->val);
+            current = current->next;
+        }
+
+        return binaryToDecimal(res);
+    }
+
+    /*
+        * 19. Remove Nth Node From End of List (Leetcode).
+        * Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+    */
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *current = head;
+        int size = findSize(current);
+
+        ListNode *res = nullptr, *dummy = nullptr;
+        int index = 0;
+        int skipIndex = size - n;
+
+        while (current) {
+            if (index != skipIndex) {
+                if (!res) {
+                    res = new ListNode(current->val);
+                    dummy = res;
+                } else {
+                    dummy->next = new ListNode(current->val);
+                    dummy = dummy->next;
+                }
+            }
+            current = current->next;
+            index++;
+        }
+
+        return res;
+    }
+
+    /*
+        * 203. Remove Linked List Elements (Leetcode).
+        * Link: https://leetcode.com/problems/remove-linked-list-elements/description/
+    */
+    ListNode* removeElements(ListNode* head, int val) {
+        if (!head) return nullptr;
+
+        ListNode * current = head;
+        ListNode *res = nullptr, *dummy = nullptr;
+        while (current) {
+            if (current->val != val) {
+                if (!res) {
+                    res = new ListNode(current->val);
+                    dummy = res;
+                } else {
+                    dummy->next = new ListNode(current->val);
+                    dummy = dummy->next;
+                }
+            }
+            current = current->next;
+        }
+
+        return res;
+    }
+
+    /*
+        * 2095. Delete the Middle Node of a Linked List (Leetcode).
+        * Link: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+    */
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head) return nullptr;
+
+        ListNode *current = head;
+        int size = findSize(current);
+
+        int mid = size /2, temp = 0;
+        ListNode *res = nullptr, *dummy = nullptr;
+        while (current) {
+            if (temp != mid) {
+                if (!res) {
+                    res = new ListNode(current->val);
+                    dummy = res;
+                } else {
+                    dummy->next = new ListNode(current->val);
+                    dummy = dummy->next;
+                }
+            }
+            current = current->next;
+            temp++;
+        }
+
+        return res;
+    }
+
+    /*
+        * 83. Remove Duplicates from Sorted List (Leetcode).
+        * Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+    */
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return nullptr;
+        ListNode *res = new ListNode(head->val);
+        ListNode *dummy = res;
+
+        while (head){
+            if (dummy->val != head->val) {
+                dummy->next = new ListNode(head->val);
+                dummy = dummy->next;
+            }
+            head = head->next;
+        }
+        return res;
+    }
+
+    /*
+        * 148. Sort List (Leetcode).
+        * Link: https://leetcode.com/problems/sort-list/description/
+    */
+    ListNode* sortList(ListNode* head) {
+        if (!head) return nullptr;
+
+        vector<int> temp;
+        ListNode *current = head;
+        while (current){
+            temp.push_back(current->val);
+            current = current->next;
+        }
+
+        sort(temp.begin(), temp.end());
+        ListNode *res = new ListNode(temp[0]);
+        ListNode *dummy = res;
+        for (int i = 1; i < temp.size(); --i) {
+            dummy->next = new ListNode(temp[i]);
+            dummy = dummy->next;
+        }
+        return res;
+    }
+
+    /*
+        * 147. Insertion Sort List (Leetcode).
+        * Link: https://leetcode.com/problems/insertion-sort-list/description/
+    */
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head) return nullptr;
+
+        vector<int> temp;
+        ListNode *current = head;
+        while (current){
+            temp.push_back(current->val);
+            current = current->next;
+        }
+
+        sort(temp.begin(), temp.end());
+        ListNode *res = new ListNode(temp[0]);
+        ListNode *dummy = res;
+        for (int i = 1; i < temp.size(); ++i) {
+            dummy->next = new ListNode(temp[i]);
+            dummy = dummy->next;
+        }
+        return res;
+    }
+
+    /*
+        * Node at a given index in linked list (Geeks for Geeks).
+        * Link: https://www.geeksforgeeks.org/problems/node-at-a-given-index-in-linked-list/1?page=1&category=Linked%20List&sprint=ca8ae412173dbd8346c26a0295d098fd&sortBy=submissions
+    */
+    int GetNth(Node *head, int index) {
+        if (!head) return -1;
+
+        Node *current = head;
+        int size = findSize(current);
+        if (index <= 0 || index > size) return -1;
+
+        int temp = 1;
+        while (current){
+            if (temp == index) return current->data;
+            current = current->next;
+            temp++;
+        }
+        return -1;
+    }
+
+    /*
+        * Find n/k th node in Linked list (Geeks for Geeks).
+        * Link: https://www.geeksforgeeks.org/problems/find-nk-th-node-in-linked-list/1?page=1&category=Linked%20List&sprint=ca8ae412173dbd8346c26a0295d098fd&sortBy=submissions
+    */
+    int fractional_node(struct Node *head, int k) {
+        Node *current = head;
+        int size = findSize(current);
+
+        if (size == 1) return current->data;
+
+        int temp = 1;
+        while (current){
+            if (temp == ceil(double(size)/k)) return current->data;
+            current = current->next;
+            temp++;
+        }
+        return -1;
+    }
+};
+
+#endif //SINGLE_LINKED_LIST_PROBLEM_SOLVING_H
---
+++ Trees/BST/Problem_Solving.h
@@ -0,0 +1,136 @@
+#ifndef BST_PROBLEM_SOLVING_H
+#define BST_PROBLEM_SOLVING_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    void inorder(TreeNode *current, vector<int> &res){
+        if (current){
+            inorder(current->left, res);
+            res.push_back(current->val);
+            inorder(current->right, res);
+        }
+    }
+
+    void inorder(Node *current, vector<int> &res){
+        if (current){
+            inorder(current->left, res);
+            res.push_back(current->data);
+            inorder(current->right, res);
+        }
+    }
+
+    void inorder(TreeNode *current, map<int, int> &mp){
+        if (current){
+            inorder(current->right, mp);
+            mp[current->val]++;
+            inorder(current->left, mp);
+        }
+    }
+
+public:
+    /*
+        * 230. Kth Smallest Element in a BST (Leetcode).
+        * Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+    */
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> vec;
+        inorder(root, vec);
+        sort(vec.begin(), vec.end());
+        return vec[k-1];
+    }
+
+    /*
+        * Median of BST (Geeks for Geeks).
+        * Link: https://www.geeksforgeeks.org/problems/median-of-bst/1?page=1&category=Binary%20Search%20Tree&difficulty=Easy&sortBy=submissions
+    */
+    float findMedian(struct Node *root) {
+        vector<int> vec;
+        inorder(root, vec);
+        sort(vec.begin(), vec.end());
+        int midIndex = vec.size() /2;
+        if (vec.size() %2 == 0) return float((vec[midIndex - 1] + vec[midIndex])) /2;
+        return vec[midIndex];
+    }
+
+    /*
+        * 501. Find Mode in Binary Search Tree (Leetcode).
+        * Link: https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+    */
+    vector<int> findMode(TreeNode* root) {
+        map<int, int> mode;
+        inorder(root, mode);
+        vector<pair<int, int>> vec;
+        for(auto m : mode){
+            vec.emplace_back(m);
+        }
+        vector<int> res;
+        sort(vec.begin(), vec.end(), [](const pair<int, int> a, pair<int, int> b){
+            return a.second > b.second;
+        });
+
+        res.push_back(vec[0].first);
+        for (int i = 1; i < vec.size(); ++i) {
+            if (vec[i].second != vec[0].second) break;
+            res.push_back(vec[i].first);
+        }
+
+        return res;
+    }
+
+    /*
+        * 783. Minimum Distance Between BST Nodes (Leetcode).
+        * Link: https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+    */
+    int minDiffInBST(TreeNode* root) {
+        vector<int> vec, res;
+        inorder(root, vec);
+        for (int i = 0; i < vec.size(); ++i) {
+            for (int j = i+1; j < vec.size(); ++j) {
+                res.push_back(abs(vec[i] - vec[j]));
+            }
+        }
+        sort(res.begin(), res.end());
+        return res[0];
+    }
+
+    /*
+        * 530. Minimum Absolute Difference in BST (Leetcode).
+        * Link: https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+    */
+    int getMinimumDifference(TreeNode* root) {
+        vector<int> vec, res;
+        inorder(root, vec);
+        for (int i = 0; i < vec.size(); ++i) {
+            for (int j = i+1; j < vec.size(); ++j) {
+                res.push_back(abs(vec[i] - vec[j]));
+            }
+        }
+        sort(res.begin(), res.end());
+        return res[0];
+    }
+};
+
+#endif //BST_PROBLEM_SOLVING_H
